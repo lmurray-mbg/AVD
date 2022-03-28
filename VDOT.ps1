c41f7494c9373498a37ace1565ef1247410ccd55
lmurray-mbg <83401697+lmurray-mbg@users.noreply.github.com>
2022-03-28 18:28:36 +0100

    Add files via upload

--- VDOT.ps1
+++ VDOT.ps1
@@ -1,5 +1,7 @@
 ﻿
-Invoke-WebRequest -Uri https://github.com/lmurray-mbg/AVD/raw/main/VDOT.zip -OutFile .\VDOT.zip -UseBasicParsing
+$uri = "https://github.com/lmurray-mbg/AVD/raw/main/VDOT.zip"
+
+Invoke-WebRequest -Uri $uri -OutFile .\VDOT.zip -UseBasicParsing
 
 Expand-Archive -Path .\VDOT.zip -DestinationPath .\
 
