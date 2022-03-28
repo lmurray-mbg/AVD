b6527682a730e09d7ca471b369ccb1233d90dfd4
lmurray-mbg <83401697+lmurray-mbg@users.noreply.github.com>
2022-03-28 17:30:06 +0100

    Add files via upload

---
+++ VDOT.ps1
@@ -0,0 +1,8 @@
+﻿
+Invoke-WebRequest -Uri https://github.com/lmurray-mbg/AVD/raw/main/VDOT.zip -OutFile .\VDOT.zip -UseBasicParsing
+
+Expand-Archive -Path .\VDOT.zip -DestinationPath .\
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+
+.\VDOT\Windows_VDOT.ps1 -Verbose -AcceptEULA
